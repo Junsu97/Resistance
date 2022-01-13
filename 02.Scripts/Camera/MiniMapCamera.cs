e18293f1b76091b55d54554d3565e4534474cbe0
Junsu97 <junsoo1230.gmail.com>
2022-01-13 13:39:05 +0900

    .

--- 02.Scripts/Camera/MiniMapCamera.cs
+++ 02.Scripts/Camera/MiniMapCamera.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class MiniMapCamera : MonoBehaviour
 {
     public Transform player;
- 
+    private void Awake()
+    {
+       if(GameObject.Find(this.name))
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+       else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     void LateUpdate()
     {
         this.transform.position = player.position + new Vector3(0, 50f, 0);
